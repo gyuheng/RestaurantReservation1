a4801e62f327865fbc3a1eace1f5091e06a1194c
yuhen <yuhen@LAPTOP-5S7VTLOD>
2023-02-08 17:01:45 +0800

    Add project files.

---
+++ RestaurantReservation1/Server/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace RestaurantReservation1.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
